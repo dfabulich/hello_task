3e661a826c04b0d0ef0d5f288fb947a5cd7e0926
Dan Fabulich <dan.fabulich@redfin.com>
2016-02-02 15:56:10 -0800

    only run the task on its target

--- plugins/hello_world/hello_world.py
+++ plugins/hello_world/hello_world.py
@@ -2,7 +2,14 @@ from pants.task.task import Task
 from pants.build_graph.target import Target
 
 class HelloWorldTask(Task):
+	@staticmethod
+	def is_hello_world_target(target):
+		return isinstance(target, HelloWorldTarget)
+
 	def execute(self):
+		targets = self.context.targets(predicate=self.is_hello_world_target)
+		if not targets:
+			return
 		self.context.log.warn("hello world")
 
 class HelloWorldTarget(Target):
